97fb1454df3e220a4088f6dd70654f55a532946a
brendasutara <sutarabrenda@gmail.com>
2024-01-10 16:08:26 -0300

    Componente: OrderCard

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Components/OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -7,7 +8,7 @@ const CheckoutSideMenu = () => {
 
   return (
     <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} top-20 checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} top-20 checkout-side-menu scrollable-cards flex-col fixed right-0 border border-gray rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>My Order</h2>
         <div onClick={()=> context.closeCheckoutSideMenu()} className='cursor-pointer'>
@@ -16,6 +17,18 @@ const CheckoutSideMenu = () => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
           </svg>
         </div>
+      </div>
+      <div className='px-6'>
+        {
+          context.cartProducts.map(product => (
+            <OrderCard
+              key={product.id}
+              title={product.title}
+              imageUrl={product.images}
+              price={product.price}
+            />
+          ))
+        }
       </div>
     </aside>
   )
