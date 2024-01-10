ef5185d86914b782e3506c5b2493d8d22dac9cb8
brendasutara <sutarabrenda@gmail.com>
2024-01-10 17:19:54 -0300

    Componente: Card-renderIcon sin duplicados

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -25,8 +25,7 @@ const Card = (data) => {
         if (isInCart) {
             return (
                 <div 
-                    className='absolute top-0 right-0 flex justify-center items-center bg-indigo-400 w-6 h-6 rounded-full m-2 p-1'
-                    onClick={(event)=> addProductsToCart(event, data.data)}>
+                    className='absolute top-0 right-0 flex justify-center items-center bg-indigo-400 w-6 h-6 rounded-full m-2 p-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#ffffff" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                     </svg>
