b786fcefdfe97d5db10b8d2d6944c6af93c8f7a0
brendasutara <sutarabrenda@gmail.com>
2024-01-14 16:03:27 -0300

    Agregar un evento click al botón de Sign up para modificar el estado local y cambiar la vista a renderizar

--- src/Pages/Signin/index.jsx
+++ src/Pages/Signin/index.jsx
@@ -20,32 +20,31 @@ function SignIn() {
         return (
             <div className='flex flex-col w-80'>
                 <p className='flex flex-col mb-6'>
-                <span className='font-medium text-sm mb-2'>Email: </span>
-                <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.email}</span>
+                    <span className='font-medium text-sm mb-2'>Email: </span>
+                    <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.email}</span>
                 </p>
                 <p className='flex flex-col'>
-                <span className='font-medium text-sm mb-2'>Password: </span>
-                <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.password}</span>
+                    <span className='font-medium text-sm mb-2'>Password: </span>
+                    <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.password}</span>
                 </p>
-                <Link
-                to="/">
+                <Link to="/">
                     <button 
-                    className='bg-indigo-500 text-white w-full rounded-lg py-3 mt-4'
-                    disabled={!hasUserAnAccount}>
-                    Log in
+                        className='bg-indigo-500 text-white w-full rounded-lg py-3 mt-4'
+                        disabled={!hasUserAnAccount}>
+                            Log in
                     </button>
-                <button
-                    className='bg-black disabled:bg-black/40 text-white  w-full rounded-lg '>
-                    
-                </button>
+                    <button
+                        className='bg-black disabled:bg-black/40 text-white  w-full rounded-lg'>   
+                    </button>
                 </Link>
                 <div className='text-center'>
-                <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
+                    <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
                 </div>
                 <button 
-                className='border-2 border-indigo-500 w-full rounded-lg mt-6 py-3'
-                disabled={hasUserAnAccount}>
-                    Sign up
+                    className='border-2 border-indigo-500 w-full rounded-lg mt-6 py-3'
+                    onClick={() => setView('create-user-info')}
+                    disabled={hasUserAnAccount}>
+                        Sign up
                 </button>
             </div>
         )
