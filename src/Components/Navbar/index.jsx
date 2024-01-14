778f52e773f70954c8784c109c8ebc6eecfed43c
brendasutara <sutarabrenda@gmail.com>
2024-01-14 12:52:35 -0300

    Correción de const renderView

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -33,33 +33,47 @@ const Navbar = () => {
             )
         } else {
             return (
-            <>
-                <li className='text-black/60'>
-                teff@platzi.com
-                </li>
-                <li>
-                <NavLink
-                    to='/my-orders'
-                    className={({ isActive }) => isActive ? activeStyle : undefined}>
-                    My Orders
-                </NavLink>
-                </li>
-                <li>
-                <NavLink
-                    to='/my-account'
-                    className={({ isActive }) => isActive ? activeStyle : undefined}>
-                    My Account
-                </NavLink>
-                </li>
-                <li>
-                <NavLink
-                    to='/sign-in'
-                    className={({ isActive }) => isActive ? activeStyle : undefined}
-                    onClick={() => handleSignOut()}>
-                    Sign out
-                </NavLink>
-                </li>
-            </>
+                <>
+                    <li className='text-black/60'>
+                        sutarabrenda@gmail.com
+                    </li>
+
+                    <li>
+                    {/* Cada NavLink para cada elemento de mi navbar */}
+                    <NavLink
+                        to='/my-orders'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }
+                        >
+                        My Orders
+                    </NavLink>
+                    </li>
+
+                    <li>
+                    {/* Cada NavLink para cada elemento de mi navbar */}
+                    <NavLink
+                        to='/my-account'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }
+                        >
+                        My Account
+                    </NavLink>
+                    </li>
+
+                    <li>
+                    {/* Cada NavLink para cada elemento de mi navbar */}
+                    <NavLink
+                        to='/sign-in'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }
+                        onClick={() => handleSignOut()}>
+                        Sign in
+                    </NavLink>
+                    </li>
+                </>
             )
         }
     }
@@ -157,46 +171,6 @@ const Navbar = () => {
             {/* Navbar derecha */}
             <ul className='flex items-center gap-3'>
                 {renderView()}
-                <li className='text-black/60'>
-                    sutarabrenda@gmail.com
-                </li>
-
-                <li>
-                {/* Cada NavLink para cada elemento de mi navbar */}
-                <NavLink
-                    to='/my-orders'
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }
-                    >
-                    My Orders
-                </NavLink>
-                </li>
-
-                <li>
-                {/* Cada NavLink para cada elemento de mi navbar */}
-                <NavLink
-                    to='/my-account'
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }
-                    >
-                    My Account
-                </NavLink>
-                </li>
-
-                <li>
-                {/* Cada NavLink para cada elemento de mi navbar */}
-                <NavLink
-                    to='/sign-in'
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }
-                    onClick={() => handleSignOut()}>
-                    Sign in
-                </NavLink>
-                </li>
-
                 <li className='flex items-center'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
