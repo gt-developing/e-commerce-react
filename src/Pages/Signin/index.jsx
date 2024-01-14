bf4646790f7a1236b80983d255d42d8298ef45d7
brendasutara <sutarabrenda@gmail.com>
2024-01-14 15:58:44 -0300

    Crear el estado local para cambiar las vistas a renderizar y usamos Conditional Rendering para crear las vistas de creación de cuenta y log in

--- src/Pages/Signin/index.jsx
+++ src/Pages/Signin/index.jsx
@@ -1,10 +1,11 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
 function SignIn() {
     const context = useContext(ShoppingCartContext)
+    const [view, setView] = useState('user-info')
 
     //Account
     const account = localStorage.getItem('account')
@@ -15,39 +16,51 @@ function SignIn() {
     const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
     const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
+    const renderLognIn = () => {
+        return (
+            <div className='flex flex-col w-80'>
+                <p className='flex flex-col mb-6'>
+                <span className='font-medium text-sm mb-2'>Email: </span>
+                <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.email}</span>
+                </p>
+                <p className='flex flex-col'>
+                <span className='font-medium text-sm mb-2'>Password: </span>
+                <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.password}</span>
+                </p>
+                <Link
+                to="/">
+                    <button 
+                    className='bg-indigo-500 text-white w-full rounded-lg py-3 mt-4'
+                    disabled={!hasUserAnAccount}>
+                    Log in
+                    </button>
+                <button
+                    className='bg-black disabled:bg-black/40 text-white  w-full rounded-lg '>
+                    
+                </button>
+                </Link>
+                <div className='text-center'>
+                <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
+                </div>
+                <button 
+                className='border-2 border-indigo-500 w-full rounded-lg mt-6 py-3'
+                disabled={hasUserAnAccount}>
+                    Sign up
+                </button>
+            </div>
+        )
+    }
+    
+    const renderCreateUserInfo = () => {
+        //TODO: Create render view
+    }
+
+    const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLognIn()
+
   return (
     <Layout>
       <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
-      <div className='flex flex-col w-80'>
-        <p className='flex flex-col mb-6'>
-          <span className='font-medium text-sm mb-2'>Email: </span>
-          <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.email}</span>
-        </p>
-        <p className='flex flex-col'>
-          <span className='font-medium text-sm mb-2'>Password: </span>
-          <span className='bg-gray-100 text-gray-500 rounded-lg h-10 p-2 font-light'>{parsedAccount?.password}</span>
-        </p>
-        <Link
-          to="/">
-            <button 
-            className='bg-indigo-500 text-white w-full rounded-lg py-3 mt-4'
-            disabled={!hasUserAnAccount}>
-            Log in
-            </button>
-          <button
-            className='bg-black disabled:bg-black/40 text-white  w-full rounded-lg '>
-            
-          </button>
-        </Link>
-        <div className='text-center'>
-          <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
-        </div>
-        <button 
-        className='border-2 border-indigo-500 w-full rounded-lg mt-6 py-3'
-        disabled={hasUserAnAccount}>
-            Sign up
-        </button>
-      </div>
+      {renderView()}
     </Layout>
   )
 }
